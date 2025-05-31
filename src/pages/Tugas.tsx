4a56204e48109471e334c4f817b2bcec1b17d939
Raihan Musthafa Kamal <114491951+raihanrpl@users.noreply.github.com>
2025-05-31 22:09:07 +0800

    first commit

---
+++ src/pages/Tugas.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { TaskView } from '../components/tasks/TaskView';
+
+export default function Tugas() {
+  return (
+    <div className="space-y-6">
+      <h1 className="text-2xl font-bold text-gray-800">Daftar Tugas</h1>
+      <TaskView />
+    </div>
+  );
+}
